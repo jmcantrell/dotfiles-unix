16f8d215931c4410b076b6e425d1befd7d2afc72
Jeremy Cantrell <jmcantrell@gmail.com>
2021-05-08 00:41:31 -0500

    config changes

--- stow/dotfiles/.config/ranger/commands.py
+++ stow/dotfiles/.config/ranger/commands.py
@@ -2,12 +2,16 @@
 
 import os
 from glob import glob
+from datetime import datetime, timedelta
+from configparser import ConfigParser
 
 import ranger
 from ranger.api.commands import Command
 from ranger.ext.shell_escape import shell_escape as esc
 
-pager = os.environ.get('PAGER', ranger.DEFAULT_PAGER)
+pager = os.environ.get("PAGER", ranger.DEFAULT_PAGER)
+
+trash_dir = os.path.expanduser("~/.local/share/Trash")
 
 
 def escape(value):
@@ -18,25 +22,47 @@ def escape(value):
     return esc(value)
 
 
-def get_trashinfo():
-    t = os.path.expanduser('~/.local/share/Trash')
-    return glob(os.path.join(t, 'info', '*.trashinfo'))
+def get_trashinfos(older_than=None):
+    infos = glob(os.path.join(trash_dir, "info", "*.trashinfo"))
+
+    if not older_than:
+        return infos
+
+    filtered_infos = []
+
+    for info in infos:
+        config = ConfigParser()
+        config.read(info)
+
+        deletion_date = datetime.fromisoformat(config["Trash Info"]["DeletionDate"])
+
+        if deletion_date < older_than:
+            filtered_infos.append(info)
+
+    return filtered_infos
 
 
 class UserCommand(Command):
-
-    command = ''
+    command = None
+    flags = None
 
     def run(self, command=None, **kwargs):
         command = command or self.command
-        if '%' in command:
+
+        if not command:
+            self.fm.notify("Command not set!", bad=True)
+            return
+
+        if "%" in command:
             command = self.fm.substitute_macros(command, escape=True)
+
+        if self.flags:
+            kwargs["flags"] = self.flags
+
         self.fm.execute_command(command, **kwargs)
 
-    def untag(self):
-        for f in self.fm.tags.tags:
-            if str(f).startswith(self.fm.thisfile.path):
-                self.fm.tags.remove(f)
+    def execute(self):
+        self.run()
 
 
 class bulkrename(UserCommand):
@@ -46,7 +72,7 @@ class bulkrename(UserCommand):
     After you edit and save the file, it rename the files accordingly.
     """
 
-    command = 'rn %s'
+    command = "br %s"
 
 
 class apack(UserCommand):
@@ -55,15 +81,14 @@ class apack(UserCommand):
     Pack the selected files using apack.
     """
 
-    command = 'apack {} %s'
-
     def execute(self):
         archive = self.rest(1)
+
         if not archive:
             self.fm.notify("Missing archive filename!", bad=True)
             return
-        command = self.command.format(archive)
-        self.run([command, archive])
+
+        self.run("apack {} %s".format(archive), flags="p")
 
     def tab(self, tabnum):
         return self._tab_directory_content()
@@ -75,7 +100,8 @@ class aunpack(UserCommand):
     Unpack the selected files using aunpack.
     """
 
-    command = 'aunpack %s'
+    command = "aunpack %s"
+    flags = "p"
 
 
 class trash(UserCommand):
@@ -84,7 +110,8 @@ class trash(UserCommand):
     Trash the selected files.
     """
 
-    command = 'trash %s'
+    command = "trash %s"
+    flags = "s"
 
     def execute(self):
         confirm = self.fm.settings.confirm_on_delete
@@ -95,27 +122,26 @@ class trash(UserCommand):
             self.fm.notify("No file selected for deletion!", bad=True)
             return
 
-        many_files = (
-            self.fm.thisdir.marked_items or (
-                cf.is_directory and not cf.is_link and len(os.listdir(cf.path)) > 0
-            )
+        many_files = self.fm.thisdir.marked_items or (
+            cf.is_directory and not cf.is_link and len(os.listdir(cf.path)) > 0
         )
 
-        if confirm != 'never' and (confirm != 'multiple' or many_files):
+        def ask_callback(answer):
+            if answer.lower() == "y":
+                self.run()
+
+        if confirm != "never" and (confirm != "multiple" or many_files):
             selection = self.fm.thistab.get_selection()
             num_files = len(selection)
             self.fm.ui.console.ask(
                 "Confirm trash of {} {}? (y/N)".format(
-                    num_files, 'files' if num_files > 1 else 'file'
+                    num_files, "files" if num_files > 1 else "file"
                 ),
-                self._ask_callback, ('n', 'N', 'y', 'Y')
+                ask_callback,
+                ("n", "N", "y", "Y"),
             )
         else:
             # no need for a confirmation
-            self.run()
-
-    def _ask_callback(self, answer):
-        if answer.lower() == 'y':
             self.run()
 
 
@@ -125,7 +151,7 @@ class trashrestore(UserCommand):
     Restore file from the trash.
     """
 
-    command = 'trash-restore'
+    command = "trash-restore"
 
 
 class trashlist(UserCommand):
@@ -134,37 +160,53 @@ class trashlist(UserCommand):
     List the trash contents.
     """
 
-    command = 'trash-list'
+    command = "trash-list"
+    flags = "p"
 
     def execute(self):
-        trashinfo = get_trashinfo()
-        if not trashinfo:
+        if not get_trashinfos():
             self.fm.notify("Trash is empty.")
             return
+
         self.run()
 
 
 class trashempty(UserCommand):
-    """:trashempty
+    """:trashempty [days]
 
     Empty the trash.
+    If days is provided, only delete files deleted before that number of days ago.
     """
 
-    command = 'trash-empty'
+    command = "trash-empty {}"
+    flags = "s"
 
     def execute(self):
-        trashinfo = get_trashinfo()
-        if not trashinfo:
+
+        arg = self.rest(1)
+
+        try:
+            days = int(arg) if arg else None
+        except ValueError:
+            self.fm.notify("Invalid days argument!", bad=True)
+            return
+
+        threshold_date = (datetime.now() - timedelta(days=days)) if days else None
+
+        trashinfos = get_trashinfos(older_than=threshold_date)
+
+        if not trashinfos:
             self.fm.notify("Trash is already empty!", bad=True)
             return
-        num = len(trashinfo)
-        self.fm.ui.console.ask(
-            "Empty trash of {} {}? (y/N)".format(
-                num, 'items' if num > 1 else 'item'
-            ),
-            self._ask_callback, 'nNyY'
-        )
 
-    def _ask_callback(self, answer):
-        if answer.lower() == 'y':
-            self.run()
+        num = len(trashinfos)
+
+        def ask_callback(answer):
+            if answer.lower() == "y":
+                self.run(self.command.format(days or ""))
+
+        self.fm.ui.console.ask(
+            "Empty trash of {} {}? (y/N)".format(num, "items" if num > 1 else "item"),
+            ask_callback,
+            "nNyY",
+        )

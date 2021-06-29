08e6ca333cb4e3d41a82a94fe9fbc6505e46d32f
Jeremy Cantrell <jmcantrell@gmail.com>
2021-06-29 12:28:35 -0500

    Trying to piece together a workflow

--- update.sh
+++ update.sh
@@ -2,5 +2,10 @@
 
 set -e
 
+export PATH=$PWD/bin:$PATH
+
 git pull origin master
 git submodule update --recursive --init
+
+generate-vim-helptags
+update-vim-rplugins
--- upgrade.sh
+++ upgrade.sh
@@ -2,4 +2,9 @@
 
 set -e
 
+export PATH=$PWD/bin:$PATH
+
 git submodule update --recursive --remote
+
+generate-vim-helptags
+update-vim-rplugins

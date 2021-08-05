7b667ffb5f8189bceb390e0737a29747986af129
Jeremy Cantrell <jmcantrell@gmail.com>
2021-08-05 05:54:13 -0500

    Ensure ssh configs have the correct permissions

--- stow.sh
+++ stow.sh
@@ -5,6 +5,14 @@ set -euo pipefail
 mkdir -p ~/.local/tmp
 chmod 700 ~/.local/tmp
 
+mkdir -p ~/.gnupg
+chmod -R 700 ~/.gnupg
+
+mkdir -p ~/.ssh
+chmod -R 700 ~/.ssh
+touch ~/.ssh/{known_hosts,authorized_keys}
+chmod 600 ~/.ssh/{id_*,known_hosts,authorized_keys}
+
 mkdir -p ~/.config/nvim/spell
 
 stow --no-folding -d . -t ~ -R stow

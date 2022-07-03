836a70ca38677ea16c6b169a70d785c67e1d33bd
Jeremy Cantrell <jmcantrell@gmail.com>
2022-07-02 22:05:22 -0500

    7z will work on iso files, apparently

--- files/home/.config/ranger/scope.sh
+++ files/home/.config/ranger/scope.sh
@@ -59,7 +59,7 @@ handle_extension() {
             ## Avoid password prompt by providing empty password
             unrar lt -p- -- "${FILE_PATH}" && exit 5
             exit 1;;
-        7z)
+        7z|iso)
             ## Avoid password prompt by providing empty password
             7z l -p -- "${FILE_PATH}" && exit 5
             exit 1;;

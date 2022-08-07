8695e3f0f3b8ae987637ad40f529cb28b0dcef3c
Jeremy Cantrell <jmcantrell@gmail.com>
2022-08-07 12:56:07 -0500

    A little cleanup and make messages consistent with direnv stdlib

--- files/home/.config/direnv/lib/python.sh
+++ files/home/.config/direnv/lib/python.sh
@@ -1,46 +1,45 @@
 use_python() {
-    local version=$1
+    local version
+    version=${1:-$(tool_version python)}
 
     if [[ -z $version ]]; then
-        log_error "missing python version"
+        log_error "Version for Python is missing"
         return 1
     fi
 
     if [[ -z ${PYTHON_VERSIONS:-} ]]; then
-        log_error "PYTHON_VERSIONS is unset or empty"
+        log_error "Environment variable PYTHON_VERSIONS is unset or empty"
         return 1
     fi
 
     local prefix=$PYTHON_VERSIONS/$PYTHON_VERSION_PREFIX$version
 
     if [[ ! -d $prefix ]]; then
-        log_error "python version $version does not exist"
+        log_error "Unable to find Python $version in $PYTHON_VERSIONS"
         return 1
     fi
 
     if [[ ! -x $prefix/bin/python ]]; then
-        log_error "python binary not found"
+        log_error "Unable to find Python binary in $prefix/bin"
         return 1
     fi
 
     load_prefix "$prefix"
 
-    log_status "loaded python $version from $prefix"
+    log_status "Successfully loaded Python $version from $prefix"
 }
 
 layout_python() {
     local python=${1:-python}
     (($# > 0)) && shift
 
-    if [[ -n ${VIRTUAL_ENV:-} ]]; then
-        local REPLY
-        realpath.absolute "$VIRTUAL_ENV"
-        VIRTUAL_ENV=$REPLY
-    else
-        local version
-        version=$("$python" -c "import platform; print(platform.python_version())")
-        VIRTUAL_ENV=$(direnv_layout_dir)/python-$version
-    fi
+    local version
+    version=$("$python" -c "import platform; print(platform.python_version())")
+
+    local layout_dir
+    layout_dir=$(direnv_layout_dir)/python-$version
+
+    VIRTUAL_ENV=$layout_dir/venv
 
     if [[ ! -d $VIRTUAL_ENV ]]; then
         "$python" -m venv "$@" "$VIRTUAL_ENV"
